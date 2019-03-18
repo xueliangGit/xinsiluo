5c978ba30b592a7cd229549b835a39ce7dbe799e
学良 <xu.xueliang@163.com>
2019-03-19 00:30:50 +0800

    'init'

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /* eslint-disable */
 /*
 const Zepto = (function () {
@@ -41,80 +43,76 @@ window.Zepto = Zepto;
 window.$ === undefined && (window.$ = Zepto);
 */
 // console.log($().method());
-$(function(){
-  init()
-$('#all').html($('.swiper-container .swiper-slide').size())
-new Swiper('.banner .swiper-container', {
-  loop: true, // 循环模式选项
+$(function () {
+  init();
+  $('#all').html($('.swiper-container .swiper-slide').size());
+  new Swiper('.banner .swiper-container', {
+    loop: true, // 循环模式选项
 
-  // 如果需要分页器
-  pagination: {
-    el: '.swiper-pagination',
-    clickable :true,
-  },
+    // 如果需要分页器
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
 
-  // 如果需要前进后退按钮
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  on:{
-    init:function(swiper){
-      console.log(this)
-          slide=this.slides.eq(0);
+    // 如果需要前进后退按钮
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    on: {
+      init: function init(swiper) {
+        console.log(this);
+        var slide = this.slides.eq(0);
         slide.addClass('ani-slide');
-
       },
-    transitionStart: function(){
-        for(var i=0;i<this.slides.length;i++){
-          slide=this.slides.eq(i);
-            slide.removeClass('ani-slide');
+      transitionStart: function transitionStart() {
+        for (var i = 0; i < this.slides.length; i++) {
+          var slide = this.slides.eq(i);
+          slide.removeClass('ani-slide');
+        }
+      },
+      transitionEnd: function transitionEnd() {
+        var slide = this.slides.eq(this.activeIndex);
+        slide.addClass('ani-slide');
+        $('#index').html(this.activeIndex);
       }
-      },
-    transitionEnd: function(){
-      slide=this.slides.eq(this.activeIndex);
-        slide.addClass('ani-slide');
-$('#index').html(this.activeIndex)
-      },
-  }
-  // 如果需要滚动条
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
+      // 如果需要滚动条
+      // scrollbar: {
+      //   el: '.swiper-scrollbar',
+      // },
+    } });
 });
-})
-function init(){
-  $(window).scroll(function(){
-    if($(window).scrollTop()>window.innerHeight*2/3){
-      $('#smallnav').addClass('show')
-      $('#top').addClass('show')
-    }else{
-      $('#smallnav').removeClass('show')
-      $('#top').removeClass('show')
+function init() {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > window.innerHeight * 1 / 3) {
+      $('#smallnav').addClass('show');
+      $('#top').addClass('show');
+    } else {
+      $('#smallnav').removeClass('show');
+      $('#top').removeClass('show');
     }
-  })
-  $('#top').click(function(){
-    $('html,body').animate({scrollTop: 0})
-  })
-  $('#close').click(function(){
-    $('#bignavshow').fadeOut()
-  })
-  $('#mune').click(function(){
-    $('#bignavshow').fadeIn()
-  })
-  var wow = new WOW(
-    {
-      boxClass:     'sloth',      
-      animateClass: 'animated',
-      offset:       0,          
-      mobile:       true,      
-      live:         true,      
-      callback:     function(box) {
-        // the callback is fired every time an animation is started
-        // the argument that is passed in is the DOM node being animated
-      },
-      scrollContainer: null // optional scroll container selector, otherwise use window
-    }
-  );
+  });
+  $('#top').click(function () {
+    $('html,body').animate({ scrollTop: 0 });
+  });
+  $('#close').click(function () {
+    $('#bignavshow').fadeOut();
+  });
+  $('#mune').click(function () {
+    $('#bignavshow').fadeIn();
+  });
+  var wow = new WOW({
+    boxClass: 'sloth',
+    animateClass: 'animated',
+    offset: 0,
+    mobile: true,
+    live: true,
+    callback: function callback(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null // optional scroll container selector, otherwise use window
+  });
   wow.init();
 }
